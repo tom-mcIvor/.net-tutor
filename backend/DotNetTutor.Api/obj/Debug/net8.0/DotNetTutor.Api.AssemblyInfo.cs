18fc146c6f2476b0d04bbdb54ede8baa43908648
thomas <ttmcivor@gmail.com>
2025-09-08 23:21:46 +1200

    Add DNS configuration files and update build artifacts

--- backend/DotNetTutor.Api/obj/Debug/net8.0/DotNetTutor.Api.AssemblyInfo.cs
+++ backend/DotNetTutor.Api/obj/Debug/net8.0/DotNetTutor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetTutor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f656b1b059f1faae5ec3c38c5a1c14baee944eb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1ee18974a51b55f32daa3f156b536181f92390")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetTutor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetTutor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
